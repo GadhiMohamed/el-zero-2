c1d5e61b827b689b2b7e663467141a96230ad740
GadhiMohamed <mohamedgadhi91@gmail.com>
2018-10-27 19:36:05 +0000

    third-commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,95 @@
-$(function(){let a=$('.header'),b=$('.slider');a.height($(window).height()),$(window).resize(function(){a.height($(window).height()),b.each(function(){$(this).css('paddingTop',($(window).height()-$('.slider li').height())/2)})}),$('.links li a').click(function(){$(this).parent().addClass('active').siblings().removeClass('active')}),b.each(function(){$(this).css('paddingTop',($(window).height()-$('.slider li').height())/2)}),b.bxSlider({pager:!1}),$('.links li a').click(function(){$('html, body').animate({scrollTop:$('#'+$(this).data('value')).offset().top},1e3)}),function c(){$('.my-slider .active').each(function(){$(this).is(':last-child')?$(this).delay(3e3).fadeOut(1e3,function(){$(this).removeClass('active'),$('.my-slider div').eq(0).addClass('active').fadeIn(),c()}):$(this).delay(3e3).fadeOut(1e3,function(){$(this).removeClass('active').next().addClass('active').fadeIn(),c()})})}(),window.mixitup('#container'),$('.shuffle li').click(function(){$(this).addClass('selected').siblings().removeClass('selected')}),$('body').niceScroll({cursorcolor:'#1abc9c',cursorwidth:'10px',zindex:9999999,cursoropacitymin:0.2,cursoropacitymax:0.8,cursorborder:'1px solid #1abc9c',scrollspeed:50})}),function(){var a=document.createElement('link');a.href='https://use.fontawesome.com/releases/v5.4.1/css/all.css',a.rel='stylesheet',a.type='text/css',document.getElementsByTagName('head')[0].appendChild(a)}(),function(){var a=document.createElement('link');a.href='https://fonts.googleapis.com/css?family=Lato:400,700',a.rel='stylesheet',document.getElementsByTagName('head')[0].appendChild(a)}();
+$(function () {
+    //Adjust Header Height
+    let myHeader = $('.header');
+    let slider = $('.slider');
+
+    myHeader.height($(window).height());
+    $(window).resize(function () {
+        myHeader.height($(window).height());
+        //Adjust BXslider Item to the center
+        slider.each(function () {
+            $(this).css(
+                'paddingTop',
+                ($(window).height() - $('.slider li').height()) / 2
+            );
+        });
+    });
+    //Links Add Active Class
+    $('.links li a').click(function () {
+        $(this)
+            .parent()
+            .addClass('active')
+            .siblings()
+            .removeClass('active');
+    });
+    //Adjust BXslider Item to the center
+    slider.each(function () {
+        $(this).css(
+            'paddingTop',
+            ($(window).height() - $('.slider li').height()) / 2
+        );
+    });
+    //Trigger the BX slider
+    slider.bxSlider({
+        pager: false
+    });
+
+    // Smooth Scroll To Div
+
+    $('.links li a').click(function () {
+        $('html, body').animate({
+            scrollTop: $('#' + $(this).data('value')).offset().top
+        },
+            1000
+        );
+    });
+    // Our Auto Slider Code
+    (function autoSlider() {
+        $('.my-slider .active').each(function () {
+            if (!$(this).is(':last-child')) {
+                $(this).delay(3000).fadeOut(1000, function () {
+                    $(this).removeClass('active').next().addClass('active').fadeIn();
+                    autoSlider();
+                });
+            } else {
+                $(this).delay(3000).fadeOut(1000, function () {
+                    $(this).removeClass('active');
+                    $('.my-slider div').eq(0).addClass('active').fadeIn();
+                    autoSlider();
+                });
+            }
+        });
+    }());
+
+    // Trigger MixItUp
+    window.mixitup('#container');
+
+    // Adjust shuffle links
+    $('.shuffle li').click(function () {
+        $(this).addClass('selected').siblings().removeClass('selected');
+    });
+    // Trigger NiceScroll
+    $("body").niceScroll({
+        cursorcolor: '#1abc9c',
+        cursorwidth: "10px",
+        zindex: 9999999,
+        cursoropacitymin: 0.2,
+        cursoropacitymax: 0.8,
+        cursorborder: "1px solid #1abc9c",
+        scrollspeed: 50,
+    });
+    (function () {
+        var css = document.createElement('link');
+        css.href = 'https://use.fontawesome.com/releases/v5.4.1/css/all.css';
+        css.rel = 'stylesheet';
+        css.type = 'text/css';
+        document.getElementsByTagName('head')[0].appendChild(css);
+    })();
+    (function () {
+        var css = document.createElement('link');
+        css.href = 'https://fonts.googleapis.com/css?family=Lato:400,700';
+        css.rel = 'stylesheet';
+        document.getElementsByTagName('head')[0].appendChild(css);
+    })();
+
+});
